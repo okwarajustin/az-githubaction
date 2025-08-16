3ded04b3d962e766125e8eb7530153244454a1e4
Mr.Professor <w.hannure@gmail.com>
2025-08-16 21:03:42 +0530

    modify rg and update backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "backendrg" # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "backendstoragefortoday"               # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    resource_group_name  = "demogithubaction" # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "storagebackupstatefile"               # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                              # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "terraform.tfstate"                    # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
